1a2e2700b46b286776a991402af5571c4a3588fa
Geordie <geordie@MacBook-Pro.local>
2022-04-24 18:41:33 -0400

    database attempt

--- lib/chat_web/channels/room_channel.ex
+++ lib/chat_web/channels/room_channel.ex
@@ -18,22 +18,22 @@ defmodule ChatWeb.RoomChannel do
   # broadcast to everyone in the current topic (room:lobby).
   @impl true
   def handle_in("shout", payload, socket) do
-    # Chat.Message.changeset(%Chat.Message{}, payload) |> Chat.Repo.insert  # insert into repo
+    Chat.Message.changeset(%Chat.Message{}, payload) |> Chat.Repo.insert  # insert into repo
     broadcast(socket, "shout", payload)
     {:noreply, socket}
   end
 
   # getting messages from the database to display
 
-#   def handle_info(:after_join, socket) do
-#   Chat.Message.get_messages()
-#   # |> Enum.reverse() # revers to display the latest message at the bottom of the page
-#   |> Enum.each(fn msg -> push(socket, "shout", %{
-#       time: msg.time,
-#       name: msg.name,
-#       message: msg.message,
-#     }) end)
-#   {:noreply, socket} # :noreply
-# end
+  def handle_info(:after_join, socket) do
+  Chat.Message.get_messages()
+  |> Enum.reverse() # revers to display the latest message at the bottom of the page
+  |> Enum.each(fn msg -> push(socket, "shout", %{
+      time: msg.time,
+      name: msg.name,
+      message: msg.message,
+    }) end)
+  {:noreply, socket} # :noreply
+end
 
 end
