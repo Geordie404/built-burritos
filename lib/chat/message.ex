80ac38e1a65413500dd7cd218e6a15ea4667107c
Geordie <geordie@MacBook-Pro.local>
2022-04-24 15:19:48 -0400

    removed database functionalitydisabled

--- lib/chat/message.ex
+++ lib/chat/message.ex
@@ -18,10 +18,10 @@ defmodule Chat.Message do
     |> validate_required([:time, :name, :message])
   end
 
-  def get_messages(limit \\ 20) do
-  Chat.Message
-  |> limit(^limit)
-  |> order_by(desc: :inserted_at)
-  |> Chat.Repo.all()
-end
+# def get_messages(limit \\ 20) do
+#   Chat.Message
+#   |> limit(^limit)
+#   |> order_by(desc: :inserted_at)
+#   |> Chat.Repo.all()
+# end
 end
